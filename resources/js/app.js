cd3e4a5fd082b127c481eaee1fc614a04f6f5bcc
ilhamfadillah <ilhamfadillah1995@gmail.com>
2019-04-07 21:51:51 +0700

    crud post

--- resources/js/app.js
+++ resources/js/app.js
@@ -9,6 +9,46 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
+import VueRouter from 'vue-router';
+Vue.use(VueRouter);
+
+import VueAxios from 'vue-axios';
+import axios from 'axios';
+
+import App from './App.vue';
+Vue.use(VueAxios, axios);
+
+
+import HomeComponent from './components/HomeComponent';
+import CreateComponent from './components/CreateComponent';
+import IndexComponent from './components/IndexComponent';
+import EditComponent from './components/EditComponent';
+
+
+const routes = [
+  {
+      name: 'home',
+      path: '/',
+      component: HomeComponent
+  },
+  {
+      name: 'create',
+      path: '/create',
+      component: CreateComponent
+  },
+  {
+      name: 'posts',
+      path: '/posts',
+      component: IndexComponent
+  },
+  {
+      name: 'edit',
+      path: '/edit/:id',
+      component: EditComponent
+  }
+];
+
+
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
@@ -20,7 +60,7 @@ window.Vue = require('vue');
 // const files = require.context('./', true, /\.vue$/i);
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+// Vue.component('example-component', require('./components/ExampleComponent.vue'));
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -28,6 +68,7 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-const app = new Vue({
-    el: '#app'
-});
+const router = new VueRouter({ mode: 'history', routes: routes});
+// const router = new VueRouter({ mode : 'history' });
+
+const app = new Vue(Vue.util.extend({router}, App)).$mount('#app');
